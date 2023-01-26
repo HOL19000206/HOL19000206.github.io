d1fe2830d38d68995c760987e589cf5d87b334e2
HOL19000206 <92788399+HOL19000206@users.noreply.github.com>
2023-01-26 13:41:46 +0000

    added intercative card

---
+++ inc/bsheader.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <script src="https://kit.fontawesome.com/70c682a6d6.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Nosifer&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link href="../css/styles.css" rel="stylesheet">
+  </head>
+  <body>
---
+++ views/index.php
@@ -0,0 +1,13 @@
+<?php include "../inc/header.php" ?>
+
+<div class="card">
+    <div class="circle"></div>
+    <div class="content">
+        <h2>Pepsi Max</h2>
+        <h3>Zero Sugar</h3>
+        <p>maximun tatse, no sabor</p>
+        <a href=".">Buy now</a>
+    </div>
+    <img src="https://pepsi.co.uk/images/default-source/products/new-cans/can_cherry.png?sfvrsn=1255fe74_2" alt="pepsi">
+</div>
+<?php include "../inc/footer.php" ?>
